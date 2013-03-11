44fab27451745661446b2a046c3ce4107111d582
Malinovsky Dmitry <damalinov@gmail.com>
2013-03-11 16:41:25 +0700

    Added valid/invalid handlers

--- subscribe.py
+++ subscribe.py
@@ -1,30 +1,60 @@
 from twilix.stanzas import Presence
 from twilix.base.myelement import EmptyStanza
+from twilix.base.exceptions import WrongElement
 
-class SubscrHandler(Presence):
+class ValidSubscrHandler(Presence):
     """
     Extends Presence class.
-    Defines handlers for subscribe queries.
+    Defines handlers for valid subscribe queries.
     """
 
+    def clean_to(self, value):
+        if self.host:
+            if not self.host.validator(value.user):
+                raise WrongElement
+        return value
+
     def availableHandler(self):
-        if self.host.validator(self.to.user):
-            return self.get_reply()
+        return self.get_reply()
 
     def probeHandler(self):
-        if self.host.validator(self.to.user):
-            reply = self.get_reply()
-            reply.to = reply.to.bare()
-            reply.type_ = 'presence'
-            return reply
-        else:
-            return EmptyStanza()
+        reply = self.get_reply()
+        reply.to = reply.to.bare()
+        reply.type_ = 'presence'
+        return reply
 
     def subscribeHandler(self):
-        if self.host.validator(self.to.user):
-            reply = self.get_reply()
-            reply.type_ = 'subscribed'
-            return reply, self.probeHandler()
+        reply = self.get_reply()
+        reply.type_ = 'subscribed'
+        return reply, self.probeHandler()
+
+    def subscribedHandler(self):
+        return EmptyStanza()
+
+    def unsubscribedHandler(self):
+        reply = Presence(to=self.from_, from_=self.to, type_='unsubscribed')
+        return reply
+
+class InvalidSubscrHandler(Presence):
+    """
+    Extends Presence class.
+    Defines handlers for invalid subscribe queries.
+    """
+
+    def clean_to(self, value):
+        if self.host:
+            if self.host.validator(value.user):
+                raise WrongElement
+        return value
+
+    def availableHandler(self):
+        return EmptyStanza()
+
+    def probeHandler(self):
+        return EmptyStanza()
+
+    def subscribeHandler(self):
+        return EmptyStanza()
 
     def subscribedHandler(self):
         return EmptyStanza()
@@ -40,4 +70,5 @@ class Subscription(object):
         self.validator = validator
 
     def init(self):
-        self.dispatcher.registerHandler((SubscrHandler, self))
+        self.dispatcher.registerHandler((ValidSubscrHandler, self))
+        self.dispatcher.registerHandler((InvalidSubscrHandler, self))
